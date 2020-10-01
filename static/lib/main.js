53f28ebd2c9d013205ab3bebf29ee5e7a0afbb7a
陈洁 <65453559+cjie95731@users.noreply.github.com>
2020-10-02 04:02:30 +0800

    Initial commit

---
+++ static/lib/main.js
@@ -0,0 +1,24 @@
+"use strict";
+
+$(document).ready(function () {
+    $("body").attr("id","screen");
+	var panel = $('<li class="js_full_screen"><a title="Full screen" href="#"><i class="screen fa fa-arrows-alt"></i><span class="visible-xs-inline">Full screen</span></a></li>');
+
+	$('ul#logged-in-menu').prepend(panel);
+	$('ul#logged-out-menu').prepend(panel);
+
+	if (utils.findBootstrapEnvironment() === 'xs') {
+		$('#menu').prepend(panel);
+	}
+	
+	panel.on('click', function () {
+		if (screenfull.enabled) {
+            screenfull.request();
+        }
+	});
+
+	panel.find('a').tooltip({
+		placement: 'bottom',
+		title: 'Full screen',
+	});
+});
